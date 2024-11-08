f6b6147bcaf7a1a5d2dae93e2a625e00fbd43b23
kamronnoriyev <kamronnoriyev7@gmail.com>
2024-11-08 12:35:34 +0500

    .

--- Program.cs
+++ Program.cs
@@ -1,4 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace g10;
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("uzbekistan");
+internal class Program
+{
+    static void Main(string[] texts)
+    {
+        //25.Rostlikga tekshiring a, b va c sonlari berilgan. 
+        //Eng kattasi a.
+
+        Console.Write("1 son kiriting : ");
+        var firstNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("2 son kiriting : ");
+        var secondNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("3 son kiriting : ");
+        var thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+        var result = (firstNumber > secondNumber && firstNumber > thirdNumber);
+
+        Console.WriteLine(result);
+
+        Main(texts);
+    }
+}
